1f7644fc1ab20b261de57d19344907fc70d0ead4
Sarah Elhefnawy <sarahelhefnawy0@gmail.com>
2024-09-20 22:44:27 +0300

    initial classes structure

---
+++ AnalysisResults.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileAnalyzerProject
+{
+	public struct AnalysisResults
+	{
+		// a struct not class cuz it has no logic just carry numbers
+		// as struct is a value type like air unlike classes that carries methods
+
+		public int WordCount;
+		public int CharcterCount;
+		public int LineCount;
+		public int FieldCount;
+	}
+}
