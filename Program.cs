1bb916091206f3e5f6dac0a1f0ae8554ac637448
Sarah Elhefnawy <sarahelhefnawy0@gmail.com>
2024-09-21 14:32:33 +0300

    get to the base class

--- Program.cs
+++ Program.cs
@@ -29,7 +29,15 @@
 					fileAnalysis = new TextFileAnalyzer();
 				}
 
+				// after i check the file extension i need to implement the methods of counting
+				// due to encapsulations to hide these results i can't call directly to Base class "FileAnalyzer" 
+				// it only return to interface which has only 2 implementations : csv, txt 
+				// when you open any of these implementations it contain implementaions from Base file to set results
+				// the interface doesn't have any access to the results
 				fileAnalysis.AnalyzeFile(file);
+
+				// conversion and casting to Base class FileAnalyzer to get the results
+				var results = ((FileAnalyzer)fileAnalysis).GetResults();
 			}
 
 		}
