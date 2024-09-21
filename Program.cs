666421632d6d0c182819b89554400fe8794b3cf4
Sarah Elhefnawy <sarahelhefnawy0@gmail.com>
2024-09-21 14:19:12 +0300

    extension class

--- Program.cs
+++ Program.cs
@@ -4,7 +4,34 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			Console.WriteLine("Welcome to File Analyzer!");
+			Console.WriteLine("Please enter folder path to analyze");
+			string inputFolder = Console.ReadLine();
+
+			// helps processing folders like FileInfo
+			DirectoryInfo directoryInfo = new DirectoryInfo(inputFolder);
+
+			if (!directoryInfo.Exists)
+			{
+				Console.WriteLine("Folder does not Exists!!!");
+				return;
+			}
+
+			IFileAnalysis fileAnalysis = null;
+
+			// gets all files inside folder
+			var filenames = directoryInfo.GetFiles();
+
+			foreach ( var file in filenames )
+			{
+				if (file.IsTextFile())
+				{
+					fileAnalysis = new TextFileAnalyzer();
+				}
+
+				fileAnalysis.AnalyzeFile(file);
+			}
+
 		}
 	}
 }
